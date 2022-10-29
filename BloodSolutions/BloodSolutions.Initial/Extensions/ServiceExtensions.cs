433965bf62bd04e63965b1733d9f7fcacf6e6dfe
hamid-rana <rana.eng.2012@gmail.com>
2022-10-29 14:49:11 +0600

    services  & component configuration

---
+++ BloodSolutions/BloodSolutions.Initial/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace BloodSolutions.Initial.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //CORS Configuration
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+        //IIS Configuration
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
